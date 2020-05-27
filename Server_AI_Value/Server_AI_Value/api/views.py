e84b44d7a6a8fd7dc8764171a52387017ca26434
eafit-201710076010 <lsanchezc@eafit.edu.co>
2020-05-27 16:30:21 -0500

    registro de user

---
+++ Server_AI_Value/Server_AI_Value/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework import Response
+from rest_framework import api_view
+
+from Server_AI_Value.api.serializers import SingUpSerializer
+
+
+@api_view(['POST',])
+def singUp_view(request):
+
+    if request.method == 'POST':
+        serializer = SingUpSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.save()
+            data['response'] = "successfully registered"
+            data['email'] = user.email
+            data['username'] = user.username
+        else:
+            data = serializer.errors
+        return Response(data)
+        

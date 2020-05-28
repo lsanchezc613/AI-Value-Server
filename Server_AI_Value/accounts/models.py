a0a4dc9e34f95e034885a4d3b4c6125bd209df23
jaromanp <33042735+jaromanp@users.noreply.github.com>
2020-05-28 16:42:10 -0500

    Cambios en el registro para tener todos los campos

--- Server_AI_Value/accounts/models.py
+++ Server_AI_Value/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='person')
+    fullname = models.CharField(max_length=50)
+    role = models.CharField(max_length=25)
+    company = models.CharField(max_length=25)
+    target = models.CharField(max_length=15)
+    sector = models.CharField(max_length=25)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=25)
+    country = models.CharField(max_length=25)
+
+
+
